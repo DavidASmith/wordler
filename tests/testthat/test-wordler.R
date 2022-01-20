ba03a40bd7ebcabcca1ca28fbdc85020e5cb0567
Gethin Davies <grd.davies@gmail.com>
2022-01-20 23:10:49 +0000

    :test_tube: Test `check_guess_hard_mode()`

--- tests/testthat/test-wordler.R
+++ tests/testthat/test-wordler.R
@@ -9,3 +9,26 @@ test_that("Repeated guess letters are compared to target without replacement", {
   # The first `S` should not be counted as in the target
   expect_equal(last_letter_in_pos$assess[[1]][[4]], "not_in_word")
 })
+
+test_that("check_guess_hard_mode works", {
+  game <- make_new_game(TRUE)
+  # First guess always valid
+  expect_true(check_guess_hard_mode(rep("A", 5), game))
+  game$guess[[1]] <- c("S", "O", "U", "L", "S")
+  game$assess[[1]] <- c("in_word", "in_position", rep("not_in_word", 3))
+  game$guess_count <- 1
+  # Invalid as O not in_position
+  expect_false(check_guess_hard_mode(c("S", "U", "O", "L", "S"), game))
+  expect_message(
+    check_guess_hard_mode(c("S", "U", "O", "L", "S"), game),
+    "2nd letter must be 'O'",
+  )
+  # Invalid as missing an S
+  expect_false(check_guess_hard_mode(c("F", "O", "I", "L", "F"), game))
+  expect_message(
+    check_guess_hard_mode(c("F", "O", "I", "L", "F"), game),
+    "'S' must be in word"
+  )
+  # Valid guess
+  expect_true(check_guess_hard_mode(c("T", "O", "A", "S", "T"), game))
+})
